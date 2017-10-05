7dc13953282472a6a0b4b26197a17741fcfa82e7
Chauveau Aurélien <32352855+DrBalf@users.noreply.github.com>
2017-10-05 12:17:53 +0200

    Add files via upload

---
+++ programme_principal.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+#include "i2c.h"
+#include "Arduino.h"
+#include "fonction.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ test_i2c.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include "i2c.h"
+#include "Arduino.h"
+
+
+void setup() {
+  
+  initI2C(122);
+
+}
+
+void loop() {
+  
+  Wire.beginTransmission(0x20);
+  Wire.write(0x35);
+  Wire.endTransmission();
+
+}
