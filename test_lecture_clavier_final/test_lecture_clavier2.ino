1eaf39978e1d5e933d16c517ce24843cb39c3483
DrBlaf <chauveau.aurelien.76@gmail.com>
2017-10-19 12:11:16 +0200

    ajout test clavier final

---
+++ test_lecture_clavier_final/test_lecture_clavier2.ino
@@ -0,0 +1,23 @@
+#include  <Wire.h>
+#include  "i2c.h"
+#include "Arduino.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+initI2C(122);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(0x22);
+  Wire.write(0xF0);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(0x22, 1);
+  while(Wire.available()){
+  short lecture = Wire.read();
+    Serial.println(lecture, HEX);
+  }
+}
